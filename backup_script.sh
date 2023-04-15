18fb2f972012608e3f9c4b29f6c6de67cc8a380d
Staubgeborener <eric.schroeder@protonmail.com>
2023-04-15 20:53:07 +0200

    New backup from 15-04-23

--- backup_script.sh
+++ backup_script.sh
@@ -1,25 +1,29 @@
-parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+#!/usr/bin/env bash
 
-#initializing
-github_token=`grep 'github_token=' $parent_path/.env | sed 's/^.*=//'`
-github_username=`grep 'github_username=' $parent_path/.env | sed 's/^.*=//'`
-github_repository=`grep 'github_repository=' $parent_path/.env | sed 's/^.*=//'`
+# Set parent directory path
+parent_path="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
 
-backup_folder=`grep 'backup_folder=' $parent_path/.env | sed 's/^.*=//'`
+# Initialize variables from .env file
+github_token="$(grep 'github_token=' "$parent_path"/.env | sed 's/^.*=//')"
+github_username="$(grep 'github_username=' "$parent_path"/.env | sed 's/^.*=//')"
+github_repository="$(grep 'github_repository=' "$parent_path"/.env | sed 's/^.*=//')"
+backup_folder="$(grep 'backup_folder=' "$parent_path"/.env | sed 's/^.*=//')"
 
-cd $parent_path
+# Change directory to parent path
+cd "$parent_path" || exit 1
 
-#check backup folder or create one
-if [ ! -d "$parent_path/$backup_folder" ]; then
-  mkdir $parent_path/$backup_folder
+# Check if backup folder exists, create one if it does not
+if [ ! -d "$backup_folder" ]; then
+  mkdir "$backup_folder"
 fi
 
-#copy important files into backup folder
-cp $(grep 'path_' $parent_path/.env | sed 's/^.*=//') $parent_path/$(grep 'backup_folder=' $parent_path/.env | sed 's/^.*=//')
+# Copy important files into backup folder
+cp "$(grep 'path_' "$parent_path"/.env | sed 's/^.*=//')" "$backup_folder"
 
-#git
+# Git commands
 git init
-git rm -rf --cached $parent_path/.env
-git add $parent_path
-git commit -m "new backup from $(date +"%d-%m-%y")"
-git push https://"$github_token"@github.com/"$github_username"/"$github_repository".git
+git rm -rf --cached "$parent_path"/.env
+git add "$parent_path"
+git commit -m "New backup from $(date +"%d-%m-%y")"
+git push "https://$github_token@$github_username/$github_repository.git"
+
