8854b53faa04ace4e014d9ff57a4ea272c74702a
Staubgeborener <eric.schroeder@protonmail.com>
2023-04-16 10:13:01 +0200

    New backup from 16-04-23

--- backup_script.sh
+++ backup_script.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# Set parent directory path
 parent_path=$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd -P)
 
 # Initialize variables from .env file
@@ -8,9 +9,10 @@ github_username=$(grep 'github_username=' "$parent_path"/.env | sed 's/^.*=//')
 github_repository=$(grep 'github_repository=' "$parent_path"/.env | sed 's/^.*=//')
 backup_folder=$(grep 'backup_folder=' "$parent_path"/.env | sed 's/^.*=//')
 
+# Change directory to parent path
 cd "$parent_path" || exit
 
-#check backup folder or create one
+# Check if backup folder exists, create one if it does not
 if [ ! -d "$parent_path/$backup_folder" ]; then
   mkdir "$parent_path/$backup_folder"
 fi
@@ -21,10 +23,9 @@ while read -r path; do
   cp "$path" "$parent_path/$backup_folder/$file"
 done < <(grep 'path_' "$parent_path"/.env | sed 's/^.*=//')
 
-#git
+# Git commands
 git init
 git rm -rf --cached "$parent_path"/.env
 git add "$parent_path"
 git commit -m "New backup from $(date +"%d-%m-%y")"
 git push https://"$github_token"@github.com/"$github_username"/"$github_repository".git
-
